80ce6efaf7cd6c38b3a05f2cfb8092063f983141
AntarticPenguin <kes99709@gmail.com>
2018-10-15 03:40:54 +0900

    Unreal Initial Commit

---
+++ Source/InfinityBlade/InfinityBladeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfinityBladeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITYBLADE_API AInfinityBladeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
